f1899285bfe4380429a5f8bda19998b1f62b8899
Lizzy <lizzy.brooks@gmail.com>
2018-12-05 11:10:11 -0800

    added the ability to break bricks!

--- breakout.js
+++ breakout.js
@@ -13,9 +13,16 @@ bricks = [];
 function setup() {
   background("black");
   createCanvas(800,700);
+  frameRate(30);
   k = new Ball(ballX,ballY,20,20,5,5); //make a new ball from the Ball class and call it b.
 
-
+for (let h=0; h < 250; h += 50){
+  for (let i = 0; i <750 ; i = i + 75){
+    let b = new Brick(brickX + i ,brickY + h, false);
+    bricks.push(b);
+  //  print(bricks);
+  }
+}
 
 
 
@@ -27,22 +34,19 @@ function draw(){
     k.drawBall();
     k.moveBall();
     k.bounceBall();
+    k.bounceBricks();
     paddle();
 
 
-for (let i=0;i<=4;i++){
+//for (let i=0;i<=4;i++){
 
-    for (let i = 0; i <750 ; i = i + 75){
-      let b = new Brick(brickX + i ,brickY);
-      bricks.push(b);
-      print(bricks);
 
-    }
-    translate(0,50);
-  }
+  //  translate(0,50);
+  // }
 
 for (let i=0;i<bricks.length; i ++ ){
-  bricks[i].drawBrick(); 
+  bricks[i].drawBrick();
+  bricks[i].breakBrick();
 }
 
 }
@@ -55,10 +59,10 @@ function paddle() {
   line(mouseX - 40, 650, mouseX + 40, 650);
 
 }
-function keyPressed() {
-  if (keyCode == 32)
-    location.reload(true);
-}
+// function keyPressed() {
+//   if (keyCode == 32)
+//     location.reload(true);
+// }
 
 function brick(x,y) {
   fill("pink");
@@ -71,21 +75,40 @@ function brick(x,y) {
 
 class Brick {
 
-	constructor(x,y) //every ball needs an x value and a y value
+	constructor(x,y, broke) //every ball needs an x value and a y value
     {
 		 this.x = x;
   	 this.y = y;
+     this.broke = broke;
 	  }
+
     drawBrick() { // draw a ball on the screen at x,y
-      fill("pink");
+
+
+      if (this.broke == true){
+        fill("black");
+
+      }
+      else if (this.broke == false){
+        fill("pink");
+
+      }
+      else {
+        fill("turquoise");
+
+      }
       strokeWeight(1);
-      rect(brickX,brickY,70,30);
-  	  }
+      rect(this.x,this.y,70,30);
+}
+
 
       breakBrick(){
-        if(k.x>=this.x && k.x<= this.x+70){
+        if(k.x>=this.x && k.x<= this.x+70 && k.y<=this.y){
+
+            this.broke = true;
+            print(this.broke);
             fill("black");
-          rect(brickX,brickY,70,30);
+            rect(this.x,this.y,70,30);
         }
       }
   }
@@ -113,30 +136,47 @@ class Ball {
 		this.y = this.y-this.speedy;
     }
   bounceBall() {
-    if (this.x >= 800)
-        {
-        this.speedx = -this.speedx;
+    // for (let i = 0; i < bricks.length; i++){
+    //     if (this.x>= bricks[i].x && this.x <= bricks[i].x+70 && this.y <= bricks[i].y ){
+    //       this.speedy = -this.speedy;
+    //     }
+
+      if (this.x >= 800)  {
+          this.speedx = -this.speedx;
         }
-    if (this.x <= 5)  {
-      this.speedx = -this.speedx;
+        if (this.x <= 5)  {
+          this.speedx = -this.speedx;
         }
-    if (this.y >= 700)  {
-      textSize(50);
-      fill(244,66,66);
-      text("GAME OVER!",240,320)
+         if (this.y >= 700)  {
+          textSize(50);
+          fill(244,66,66);
+          text("GAME OVER!",240,320)
        }
 
-    if (this.y <= 5)
-        {
-      this.speedy = -this.speedy;
+       if (this.y <= 5){
+         this.speedy = -this.speedy;
         }
-    if (this.x >= mouseX - 50 && this.x <= mouseX + 50 && this.y >= 650 - 12 && this.y <= 650 + 12) {
-      this.speedy = -this.speedy;
+
+       if (this.x >= mouseX - 50 && this.x <= mouseX + 50 && this.y >= 650 - 12 && this.y <= 650 + 12) {
+           this.speedy = -this.speedy;
       // this.speedx = -this.speedx;
-      console.log(this.speedx);
-      console.log(this.speedy);
+        console.log(this.speedx);
+        console.log(this.speedy);
+        }
+
+        // else{
+        //   print ("catch");
+        // }
+      // }
+  }
+  bounceBricks(){
+    for (let i = 0; i < bricks.length; i++){
+      if (this.x>= bricks[i].x && this.x <= bricks[i].x+70 && this.y <= bricks[i].y && bricks[i].broke == false ){
+        this.speedy = -this.speedy;
+      }
 
     }
+
   }
 
 }
